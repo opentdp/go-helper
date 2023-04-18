9d8c79bed5dc23390514b2a7ead7b33c99d8df89
若海 <wang@rehiy.com>
2023-04-18 14:20:13 +0800

    feat: 新增更新说明

--- upgrade/typing.go
+++ upgrade/typing.go
@@ -13,6 +13,7 @@ type UpdateInfo struct {
 	BinaryUrl string `note:"下载地址"`
 	Message   string `note:"错误信息"`
 	Version   string `note:"最新版本"`
+	Release   string `note:"更新说明"`
 }
 
 var ErrNoUpdate = errors.New("no update")
