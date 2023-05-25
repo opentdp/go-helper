b8f0699d057decc22fbb69173f84e9a0c3232648
若海 <wang@rehiy.com>
2023-05-25 21:37:59 +0800

    feat: 支持嵌入式文件

--- httpd/static.go
+++ httpd/static.go
@@ -1,6 +1,10 @@
 package httpd
 
 import (
+	"embed"
+	"io/fs"
+	"net/http"
+
 	"github.com/gin-contrib/static"
 )
 
@@ -15,5 +19,12 @@ func StaticIndex(urlPrefix, dir string) {
 
 	lf := static.LocalFile(dir, true)
 	engine.Use(static.Serve(urlPrefix, lf))
+
+}
+
+func StaticEmbed(urlPrefix, dir string, efs *embed.FS) {
+
+	ui, _ := fs.Sub(efs, dir)
+	engine.StaticFS(urlPrefix, http.FS(ui))
 
 }
