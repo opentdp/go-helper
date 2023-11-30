58ed21b4943de39beba49d32eebeb4d4d23b3078
若海 <wang@rehiy.com>
2023-11-30 14:11:45 +0800

    fix: 更改为二进制读写

--- filer/file.go
+++ filer/file.go
@@ -11,7 +11,7 @@ type FileInfo struct {
 	Mode    os.FileMode // 权限，如：0777
 	ModTime int64       // 修改时间，Unix时间戳
 	IsDir   bool        // 是否是目录
-	Content string      // 文本内容，仅用于文本文件
+	Data    []byte      // 文件数据
 }
 
 // 列出目录中的所有文件
@@ -58,28 +58,19 @@ func Detail(path string, text bool) (*FileInfo, error) {
 	}
 
 	if text && !info.IsDir() {
-		content, err := os.ReadFile(path)
+		data, err := os.ReadFile(path)
 		if err != nil {
 			return nil, err
 		}
-		detail.Content = string(content)
+		detail.Data = data
 	}
 
 	return detail, nil
 
 }
 
-// 读取文本内容
-func Read(path string) (string, error) {
-
-	data, err := os.ReadFile(path)
-
-	return string(data), err
-
-}
-
-// 写入文本内容
-func Write(path string, content string) error {
+// 写入文件内容
+func Write(path string, data []byte) error {
 
 	// 创建目录
 	if dir := filepath.Dir(path); !Exists(dir) {
@@ -89,7 +80,7 @@ func Write(path string, content string) error {
 	}
 
 	// 写入内容
-	return os.WriteFile(path, []byte(content), 0644)
+	return os.WriteFile(path, data, 0644)
 
 }
 
