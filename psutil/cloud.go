bf64df60769cb1675ee68d5d9dbe217436d99281
若海 <wang@rehiy.com>
2023-12-05 08:48:17 +0800

    fix: 更正变量名

--- psutil/cloud.go
+++ psutil/cloud.go
@@ -11,50 +11,50 @@ import (
 func CloudInstanceId() string {
 
 	var url string
-	var res string
+	var mid string
 
 	// alibaba
 	url = "http://100.100.100.200/latest/meta-data/instance-id"
-	res = request.TimingGet(url, request.H{}, 3)
-	if res != "" {
-		return strings.TrimSpace(res)
+	mid = request.TimingGet(url, request.H{}, 3)
+	if mid != "" {
+		return strings.TrimSpace(mid)
 	}
 
 	// tencent
 	url = "http://metadata.tencentyun.com/latest/meta-data/instance-id"
-	res = request.TimingGet(url, request.H{}, 3)
-	if res != "" {
-		return strings.TrimSpace(res)
+	mid = request.TimingGet(url, request.H{}, 3)
+	if mid != "" {
+		return strings.TrimSpace(mid)
 	}
 
 	// aws baidu huawei
 	url = "http://169.254.169.254/latest/meta-data/instance-id"
-	res = request.TimingGet(url, request.H{}, 3)
-	if res != "" {
-		return strings.TrimSpace(res)
+	mid = request.TimingGet(url, request.H{}, 3)
+	if mid != "" {
+		return strings.TrimSpace(mid)
 	}
 
 	// azure
 	url = "http://169.254.169.254/metadata/instance/compute/vmId?api-version=2021-01-01"
-	res = request.TimingGet(url, request.H{"Metadata": "true"}, 3)
-	if res != "" {
-		return strings.TrimSpace(res)
+	mid = request.TimingGet(url, request.H{"Metadata": "true"}, 3)
+	if mid != "" {
+		return strings.TrimSpace(mid)
 	}
 
 	// google
 	url = "http://metadata.google.internal/computeMetadata/v1/instance/id"
-	res = request.TimingGet(url, request.H{"Metadata-Flavor": "Google"}, 3)
-	if res != "" {
-		return strings.TrimSpace(res)
+	mid = request.TimingGet(url, request.H{"Metadata-Flavor": "Google"}, 3)
+	if mid != "" {
+		return strings.TrimSpace(mid)
 	}
 
 	// digitalocean
 	url = "http://169.254.169.254/metadata/v1/id"
-	res = request.TimingGet(url, request.H{}, 3)
-	if res != "" {
-		return strings.TrimSpace(res)
+	mid = request.TimingGet(url, request.H{}, 3)
+	if mid != "" {
+		return strings.TrimSpace(mid)
 	}
 
-	return res
+	return mid
 
 }
