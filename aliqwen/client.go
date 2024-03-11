f2ec0e18fcd2813bb854454abe7c3a79fad6e7a7
若海 <wang@rehiy.com>
2024-03-12 00:37:53 +0800

    feat: 添加通义千问模型

---
+++ aliqwen/client.go
@@ -0,0 +1,54 @@
+package aliqwen
+
+import (
+	"encoding/json"
+
+	"github.com/opentdp/go-helper/request"
+)
+
+type Client struct {
+	ApiBaseUrl string
+	ApiVersion string
+	ApiKey     string
+	Model      string
+	Params     Parameters
+}
+
+func NewClient(key string) *Client {
+
+	return &Client{
+		ApiBaseUrl: ApiBaseUrl,
+		ApiVersion: ApiVersion,
+		ApiKey:     key,
+		Model:      "qwen-max",
+		Params:     Parameters{EnableSearch: true},
+	}
+
+}
+
+func (c *Client) CreateChatCompletion(messages []Messages) (*ResponseBody, error) {
+
+	req := RequestBody{
+		Model:      c.Model,
+		Input:      Input{messages},
+		Parameters: c.Params,
+	}
+	body, _ := json.Marshal(req)
+
+	heaner := request.H{
+		"Content-Type":  "application/json",
+		"Authorization": "Bearer " + c.ApiKey,
+	}
+
+	url := c.ApiBaseUrl + "/api/" + c.ApiVersion + "/services/aigc/text-generation/generation"
+	response, err := request.Post(url, string(body), heaner)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp ResponseBody
+	err = json.Unmarshal(response, &resp)
+
+	return &resp, err
+
+}
---
+++ aliqwen/model.go
@@ -0,0 +1,48 @@
+package aliqwen
+
+// 开通地址 https://dashscope.console.aliyun.com/apiKey
+
+const ApiBaseUrl = "https://dashscope.aliyuncs.com"
+const ApiVersion = "v1"
+
+const ChatMessageRoleAssistant = "assistant"
+const ChatMessageRoleUser = "user"
+
+// request
+
+type RequestBody struct {
+	Model      string     `json:"model"`
+	Input      Input      `json:"input"`
+	Parameters Parameters `json:"parameters"`
+}
+
+type Input struct {
+	Messages []Messages `json:"messages"`
+}
+
+type Parameters struct {
+	EnableSearch bool `json:"enable_search"`
+}
+
+type Messages struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+// response
+
+type ResponseBody struct {
+	Output    Output `json:"output"`
+	Usage     Usage  `json:"usage"`
+	RequestID string `json:"request_id"`
+}
+
+type Output struct {
+	Text         string `json:"text"`
+	FinishReason string `json:"finish_reason"`
+}
+
+type Usage struct {
+	OutputTokens int `json:"output_tokens"`
+	InputTokens  int `json:"input_tokens"`
+}
