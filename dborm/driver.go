1091ccf79e23cf916695c695c97a92b43b25ca46
若海 <wang@rehiy.com>
2024-04-07 18:59:01 +0800

    fix: 添加正确的sqlite默认参数

--- dborm/driver.go
+++ dborm/driver.go
@@ -10,6 +10,22 @@ import (
 	"gorm.io/gorm"
 )
 
+func useMysql(args *Config) gorm.Dialector {
+
+	host := args.Host
+	user := args.User
+	password := args.Password
+	dbname := args.DbName
+
+	option := args.Option
+	if option == "" {
+		option = "?charset=utf8mb4&parseTime=True&loc=Local"
+	}
+
+	return mysql.Open(user + ":" + password + "@tcp(" + host + ")/" + dbname + option)
+
+}
+
 func useSqlite(args *Config) gorm.Dialector {
 
 	dbname := args.DbName
@@ -17,24 +33,13 @@ func useSqlite(args *Config) gorm.Dialector {
 		dbname = path.Join(args.Host, dbname)
 	}
 
-	os.MkdirAll(filepath.Dir(dbname), 0755)
-
-	return sqlite.Open(dbname + args.Option)
-
-}
-
-func useMysql(args *Config) gorm.Dialector {
-
-	host := args.Host
-	user := args.User
-	password := args.Password
-	dbname := args.DbName
 	option := args.Option
-
 	if option == "" {
-		option = "?charset=utf8mb4&parseTime=True&loc=Local"
+		option = "?_pragma=busy_timeout(5000)&_pragma=journal_mode(WAL)"
 	}
 
-	return mysql.Open(user + ":" + password + "@tcp(" + host + ")/" + dbname + option)
+	os.MkdirAll(filepath.Dir(dbname), 0755)
+
+	return sqlite.Open(dbname + option)
 
 }
